8e6723cea84f667ebf1fb4d6d8c02dadab4c2940
abdullapro2006 <abdullahmanafli38@gmail.com>
2024-03-11 12:07:47 +0400

    ProductArePartialView added

---
+++ Contollers/Client/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedStore.Contollers.Client;
+
+public class ProductController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult SingleProduct(int id)
+    {
+        return View();
+    }
+}
